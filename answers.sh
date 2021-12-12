8ee7b4a19372fddc5b2ef0e220074679a1baee5d
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2021-12-13 01:06:12 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -45,8 +45,30 @@ WHERE name="Layal"
 
 #3
 UPDATE graduates 
-SET Graduation = 2018-09-08 
+SET Graduation = DATE('2018-09-08')
 WHERE Name = "Layal";
 
 #4
 DELETE FROM students WHERE name="Layal";
+
+#Joins
+
+#1
+SELECT employees.Name, employees.Company, companies.Date
+FROM employees
+JOIN companies
+ON employees.Company = companies.Name;
+
+#2
+SELECT employees.Name
+FROM employees
+JOIN companies
+ON employees.Company = companies.Name
+WHERE companies.Date > 2000;
+
+#3
+SELECT companies.Name
+FROM companies
+JOIN employees
+ON employees.Company = companies.Name
+WHERE employees.Role = "Graphic Designer";
