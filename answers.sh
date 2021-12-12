8d9839c8f7a6b2af329112c1c7558487ee379c42
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2021-12-12 21:13:05 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -7,7 +7,7 @@ SELECT Name FROM students;
 SELECT * FROM students WHERE Age>30;
 
 #3
-SELECT * FROM students WHERE Age>30 AND Gender="F";
+SELECT * FROM students WHERE Age=30 AND Gender="F";
 
 #4
 SELECT Points FROM students WHERE Name="Alex";
@@ -17,10 +17,36 @@ INSERT INTO students VALUES(7,"Zaynab",21,"F",500);
 
 #6
 UPDATE students 
-SET Points = 400 
+SET Points = Points + 100 
 WHERE Name = "Basma";
 
 #7
 UPDATE students 
-SET Points = 100 
+SET Points = Points - 100 
 WHERE Name = "Alex";
+
+#Creating Tables
+
+#1
+CREATE TABLE graduates(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+    Age INT,
+    Gender TEXT,
+    Points INT,
+    Graduation DATE
+);
+
+#2
+INSERT INTO graduates(Name,Age,Gender,Points)
+SELECT name,Age,Gender,Points
+FROM students
+WHERE name="Layal"
+
+#3
+UPDATE graduates 
+SET Graduation = 2018-09-08 
+WHERE Name = "Layal";
+
+#4
+DELETE FROM students WHERE name="Layal";
